fb338b01b2029b86f869cee626c00d7911f3329e
Carloxxz <carloxmz@gmail.com>
2024-05-17 19:20:42 -0600

    - Agrega método para mostrar una imagen en caso de ser null

--- AprendiendoReact/src/components/articles.tsx
+++ AprendiendoReact/src/components/articles.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react"
-import { ApiResponse } from "../Interfaces"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import { ApiResponse } from "../Interfaces";
+import axios from "axios";
+import { Global } from "../Global";
 
 export default function Articles() {
     const [loading, setLoading] = useState(true);
     const [articleApi, setArticleApi] = useState<ApiResponse>({ status: '', articles: [] });
 
     useEffect(() => {
-        axios.get<ApiResponse>('http://localhost:3900/api/articles')
+        axios.get<ApiResponse>(`${Global.url}articles`)
             .then((res) => {
-                console.log(res.data)
                 setArticleApi(res.data);
                 setLoading(false); // Cambiar el estado de carga cuando se carguen los datos
             })
@@ -19,20 +19,37 @@ export default function Articles() {
             });
     }, []);
 
+    // Función para renderizar los artículos
+    const renderArticles = () => {
+        return (
+            articleApi.articles.map((article) => (
+                <article className="article-item" id="article-template" key={article._id}>
+                    <div className="image-wrap">
+                        {
+                            article.image !== null ? (
+                                <img src={`${Global.url}get-img/${article.image}`} alt={article.title} />
+                            ) : (
+                                <img src="https://w7.pngwing.com/pngs/819/548/png-transparent-photo-image-landscape-icon-images-thumbnail.png" alt={article.title} />
+                            )
+                        }
+                    </div>
+                    <h2>{article.title}</h2>
+                    <span className="date">{article.date}</span>
+                    <a href="#">Leer más</a>
+                    <div className="clearfix"></div>
+                </article>
+            ))
+        );
+    };
 
     return (
         <div id="articles">
             {loading ? (
                 <p>Cargando...</p> // Mostrar el mensaje de carga si loading es true
+            ) : articleApi.status === 'success' && articleApi.articles.length >= 1 ? (
+                renderArticles() // Llamar a la función renderArticles si hay artículos disponibles
             ) : (
-                // Mostrar los artículos si loading es false y hay artículos disponibles
-                articleApi.status === 'success' && articleApi.articles.length >= 1 ? (
-                    articleApi.articles.map((article) => (
-                        <h1 key={article._id}>{article.title}</h1>
-                    ))
-                ) : (
-                    <h1>No hay artículos disponibles</h1> // Mostrar un mensaje si no hay artículos disponibles
-                )
+                <h1>No hay artículos disponibles</h1> // Mostrar un mensaje si no hay artículos disponibles
             )}
         </div>
     );
